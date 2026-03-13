2531ac4899ed3fb615c43b745cb1969d32461405
lixy <lixy@anmeng.net>
2026-03-13 15:56:37 +0800

    1

--- builder/build.sh
+++ builder/build.sh
@@ -33,12 +33,23 @@ PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
-mkdir -p /workspace/builder/redroid && cd /workspace/builder/redroid
+builder_dir=/workspace/builder
+redroid_dir=${builder_dir}/redroid
+redroid_patch_dir=${builder_dir}/redroid-patches
+
+mkdir -p $redroid_dir && cd $redroid_dir
 
 # check supported branch in https://github.com/remote-android/redroid-patches.git
 repo init -u https://android.googlesource.com/platform/manifest --git-lfs --depth=1 -b android-11.0.0_r48
 
 # add local manifests
-git clone https://github.com/remote-android/local_manifests.git /workspace/builder/redroid/.repo/local_manifests -b 11.0.0
+git clone https://github.com/remote-android/local_manifests.git ${redroid_dir}/.repo/local_manifests -b 11.0.0
+
+# sync code
+repo sync -c
+
+# apply redroid patches
+git clone https://github.com/remote-android/redroid-patches.git $redroid_patch_dir
+${redroid_patch_dir}/apply-patch.sh ${redroid_dir}
 
 echo '输出的内容'
