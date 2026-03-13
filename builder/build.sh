e6b10af2271ff8e077c4e3cce15fa76e56bb1fb9
lixy <lixy@anmeng.net>
2026-03-13 15:37:19 +0800

    1

--- builder/build.sh
+++ builder/build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+
+apt-get update
+apt-get install -qq -y ca-certificates
+
+apt-get install -qq -y --no-install-recommends \
+    curl
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+mkdir -p /workspace/builder/redroid && cd mkdir /workspace/builder/redroid
+
+# check supported branch in https://github.com/remote-android/redroid-patches.git
+repo init -u https://android.googlesource.com/platform/manifest --git-lfs --depth=1 -b android-11.0.0_r48
+
+# add local manifests
+git clone https://github.com/remote-android/local_manifests.git /workspace/builder/redroid/.repo/local_manifests -b 11.0.0
+
 echo '输出的内容'
