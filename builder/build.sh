8fd6106548c0b90fa854f6d343b40bccefe37737
lixy <lixy@anmeng.net>
2026-03-13 17:58:10 +0800

    1

--- builder/build.sh
+++ builder/build.sh
@@ -2,31 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get install -qq -y ca-certificates
-
-apt-get install -qq -y --no-install-recommends \
-  acl aptly aria2 axel bc binfmt-support binutils-aarch64-linux-gnu bison \
-  bsdextrautils btrfs-progs build-essential busybox ca-certificates ccache \
-  clang coreutils cpio crossbuild-essential-arm64 cryptsetup curl \
-  debian-archive-keyring debian-keyring debootstrap device-tree-compiler \
-  dialog dirmngr distcc dosfstools dwarves e2fsprogs expect f2fs-tools \
-  fakeroot fdisk file flex gawk gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi \
-  gdisk git gnupg gzip htop imagemagick jq kmod lib32ncurses-dev \
-  lib32stdc++6 libbison-dev libc6-dev-armhf-cross libc6-i386 libcrypto++-dev \
-  libelf-dev libfdt-dev libfile-fcntllock-perl libfl-dev libfuse-dev \
-  libgcc-12-dev-arm64-cross libgmp3-dev liblz4-tool libmpc-dev libncurses-dev \
-  libncurses5 libncurses5-dev libncursesw5-dev libpython2.7-dev \
-  libpython3-dev libssl-dev libusb-1.0-0-dev linux-base lld llvm locales \
-  lsb-release lz4 lzma lzop make mtools ncurses-base ncurses-term \
-  nfs-kernel-server ntpdate openssl p7zip p7zip-full parallel parted patch \
-  patchutils pbzip2 pigz pixz pkg-config pv python2 python2-dev python3 \
-  python3-dev python3-distutils python3-pip python3-setuptools \
-  python-is-python3 qemu-user-static rar rdfind rename rsync sed \
-  squashfs-tools swig tar tree u-boot-tools udev unzip util-linux uuid \
-  uuid-dev uuid-runtime vim wget whiptail xfsprogs xsltproc xxd xz-utils \
-  zip zlib1g-dev zstd binwalk ripgrep sudo openssh-client libxml2-utils
-
 mkdir -p ~/bin
 PATH=~/bin:$PATH
 
@@ -36,7 +11,7 @@ git config --global user.name "lixy"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
-builder_dir=/workspace/builder
+builder_dir=/builder
 redroid_dir=${builder_dir}/redroid
 redroid_patch_dir=${builder_dir}/redroid-patches
 
