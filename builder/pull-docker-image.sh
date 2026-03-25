346dfc876fc56de6deb966ce2e06bd0dcbf61c49
LiXiaoYuan <1064997559@qq.com>
2026-03-25 22:40:36 +0800

    Save Docker image to release directory

--- builder/pull-docker-image.sh
+++ builder/pull-docker-image.sh
@@ -4,6 +4,8 @@ docker pull erstt/redroid:15.0.0_ndk_magisk_AVD
 
 docker images
 
-docker save erstt/redroid:15.0.0_ndk_magisk_AVD | gzip > redroid_15_ndk_magisk.tar.gz
+mkdir release
+
+docker save erstt/redroid:15.0.0_ndk_magisk_AVD | gzip > release/redroid_15_ndk_magisk.tar.gz
 
 ls -alh 
