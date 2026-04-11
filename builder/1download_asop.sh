767276476dc95eb9735c771893b4c0945a940eda
LiXiaoYuan <1064997559@qq.com>
2026-04-12 07:13:23 +0800

    Refactor redroid download and setup process
    Removed old download and setup process for redroid. Added new commands to initialize and sync the repo with git-lfs support.

--- builder/1download_asop.sh
+++ builder/1download_asop.sh
@@ -13,28 +13,9 @@ apt-get install -qq -y --no-install-recommends \
 git config --global user.email "lixiaoyuan52@gmail.com"
 git config --global user.name "lixy"
 
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
+mkdir ~/redroid && cd ~/redroid
+sudo repo init -u https://github.com/redroid-rockchip/platform_manifests.git -b redroid-12.0.0 --depth=1 --git-lfs
+sudo repo sync -c
 
-builder_dir=/builder
-redroid_dir=${builder_dir}/redroid
-redroid_patch_dir=${builder_dir}/redroid-patches
-
-mkdir -p $redroid_dir && cd $redroid_dir
-
-# check supported branch in https://github.com/remote-android/redroid-patches.git
-repo init -u https://android.googlesource.com/platform/manifest --git-lfs --depth=1 -b android-11.0.0_r48
-
-# add local manifests
-git clone https://github.com/remote-android/local_manifests.git ${redroid_dir}/.repo/local_manifests -b 11.0.0
-
-echo 'sync -c'
-
-# sync code
-repo sync -c
-
-echo 'apply redroid patches'
-
-# apply redroid patches
-git clone https://github.com/remote-android/redroid-patches.git $redroid_patch_dir
-${redroid_patch_dir}/apply-patch.sh ${redroid_dir}
+sudo apt install git-lfs
+sudo repo forall -g lfs -c git lfs pull
